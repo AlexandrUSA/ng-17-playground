aa8e432357162760072e6cdd602a0a51065e3c96
AliaksandrHrynevich <a.hrynevich@godeltech.com>
2023-12-28 22:03:41 +0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/view/login/login.component').then(c => c.LoginComponent),
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./pages/products/view/products/products.component').then(c => c.ProductsComponent),
+  },
+  {
+    path: 'products/:id',
+    loadComponent: () =>
+      import('./pages/product-details/view/product-details/product-details.component').then(
+        c => c.ProductDetailsComponent,
+      ),
+  },
+];
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  api: { baseUrl: 'http://localhost:3000/api/v1' },
+};
